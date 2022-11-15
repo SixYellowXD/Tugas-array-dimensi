224d6c4a0b809535c7715a8fd95a359ccd42e045
SixYellowXD <116617405+SixYellowXD@users.noreply.github.com>
2022-11-15 16:20:17 +0700

    Tugas array
    Tugas array satu dimensi dan dua dimensi

---
+++ tugas array satu dimensi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	cout<< "Nama 	: Ahmad Al-Hafidz Ramadhan\n";
+	cout <<"NIM 	: 22343034\n";
+	
+int Nilai[5] ={2,3,4,5,6};
+	for(int i=0; i<5;i++){
+          cout<<"Menampilkan nilai indek ke ["<<i<<"] = "<<Nilai[i];
+          cout<<endl;
+}
+      
+
+     return 0;
+}
+
